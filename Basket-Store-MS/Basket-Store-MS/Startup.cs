c7cfe8556168f7ab6b18b221fbfd7f723901ea2c
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-05-13 22:19:42 +0200

    Add Swagger

--- Basket-Store-MS/Basket-Store-MS/Startup.cs
+++ Basket-Store-MS/Basket-Store-MS/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,7 +36,7 @@ namespace Basket_Store_MS
                 string connectionString = Configuration.GetConnectionString("DefaultConnection");
                 options.UseSqlServer(connectionString);
             });
-         services.AddTransient<ICart, CartService>();
+            services.AddTransient<ICart, CartService>();
             services.AddTransient<IPaymentType,PaymentTypeServices>();
             services.AddTransient<IFeedBack, FeedBackServices>();
             services.AddTransient<IProuduct, ProuductServices>();
@@ -44,6 +45,16 @@ namespace Basket_Store_MS
                    .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
             services.AddControllers();
+            //configure a new service dependency
+            services.AddSwaggerGen(options =>
+            {
+                // Make sure get the "using Statement"
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Basket Store MS",
+                    Version = "v1",
+                });
+            });
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -52,9 +63,14 @@ namespace Basket_Store_MS
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
-
+            app.UseSwagger(options => {
+                options.RouteTemplate = "/api/{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/api/v1/swagger.json", "Basket Store MS");
+                options.RoutePrefix = "";
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -63,6 +79,7 @@ namespace Basket_Store_MS
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+           
         }
     }
 }

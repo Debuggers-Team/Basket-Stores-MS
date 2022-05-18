81238f14a96b2fb35751b5eacb60ee781d54f62d
Mohammad sarairha <mohammadsarairha@gmail.com>
2022-05-18 21:26:26 +0300

    Change email structure send it as table to user email

--- Basket-Store-MS/Basket-Store-MS/Models/DTO/BillDto.cs
+++ Basket-Store-MS/Basket-Store-MS/Models/DTO/BillDto.cs
@@ -6,8 +6,10 @@ namespace Basket_Store_MS.Models.DTO
     {
         public string UserName { get; set; }
         public double TotalCost { get; set; }
+        public double TotalCostDicount { get; set; }
         public int TotalQuantity { get; set; }
-        public string Products { get; set; }
         public string Email { get; set; }
+        public List<ProductDto> Products { get; set; }
+        public List<int> Quantity { get; set; }
     }
 }
--- Basket-Store-MS/Basket-Store-MS/Models/DTO/CartDto.cs
+++ Basket-Store-MS/Basket-Store-MS/Models/DTO/CartDto.cs
@@ -8,6 +8,7 @@ namespace Basket_Store_MS.Models.DTO
         public double TotalCost { get; set; }
         public string State { get; set; }
         public int TotalQuantity { get; set; }
+        public double TotalCostDicount { get; set; }
         public string UserId { get; set; }
         // To Many to many Relation between Cart And Product
         public List<ProductDto> Products { get; set; }
